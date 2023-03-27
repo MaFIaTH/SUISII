c9f001cebbe4e3bb8f3fe18c2840cb71edbc19c3
MaFIaTH <mafeeyawat@gmail.com>
2023-03-28 00:24:22 +0700

    Clean code + Minor addition
    - Change players and ground to Jumpable layer
    - Add Max Move Velocity to clamp movement speed.

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,49 +1,49 @@
-using System;
+using System.Linq;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D playerRigid;
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float maxMoveVelocity = 7f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private KeyCode jumpKey = KeyCode.W;
     [SerializeField] private string axisName = "Horizontal P1";
 
     [SerializeField] private LayerMask platformLayerMask;
-    [SerializeField] float extraHeightText = 0.1f;
-    private bool _canJump = true;
-    private BoxCollider2D boxCollider2d;
+    [SerializeField] private float raycastDistance = 0.1f;
+    private BoxCollider2D _boxCollider2d;
 
 
     private void Awake()
     {
-        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        _boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
 
     private void Move()
     {
         Vector2 movement = new Vector2(Input.GetAxis(axisName) * moveSpeed * Time.deltaTime, 0);
+        playerRigid.velocity = new Vector2(Mathf.Clamp(playerRigid.velocity.x, -maxMoveVelocity, maxMoveVelocity),
+            playerRigid.velocity.y);
+        Debug.Log(playerRigid.velocity);
         playerRigid.AddForce(movement);
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d .bounds.size, 0f, Vector2.down, extraHeightText, platformLayerMask);
-        Color rayColor;
-        if (raycastHit.collider != null)
-        {
-            rayColor = Color.green;
-        }
-        else
-        {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(boxCollider2d.bounds.center + new Vector3(boxCollider2d.bounds.extents.x,0), Vector2.down*(boxCollider2d.bounds.extents.y + extraHeightText),rayColor);
-        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x,0), Vector2.down*(boxCollider2d.bounds.extents.y + extraHeightText),rayColor);
-        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, boxCollider2d.bounds.extents.y), Vector2.right*(boxCollider2d.bounds.extents.x),rayColor);
-        
-        Debug.Log(raycastHit.collider);
-        return raycastHit.collider != null;
+        var bounds = _boxCollider2d.bounds;
+        RaycastHit2D[] raycastHit = Physics2D.BoxCastAll(bounds.center, bounds.size, 0f,
+            Vector2.down, raycastDistance, platformLayerMask);
+        bool hit = raycastHit.Any(x => x.collider != _boxCollider2d);
+        Color rayColor = hit ? Color.green : Color.red;
+        Debug.DrawRay(_boxCollider2d.bounds.center + new Vector3(bounds.extents.x, 0),
+            Vector2.down * (bounds.extents.y + raycastDistance), rayColor);
+        Debug.DrawRay(_boxCollider2d.bounds.center - new Vector3(bounds.extents.x, 0),
+            Vector2.down * (bounds.extents.y + raycastDistance), rayColor);
+        Debug.DrawRay(
+            _boxCollider2d.bounds.center - new Vector3(bounds.extents.x, bounds.extents.y),
+            Vector2.right * bounds.extents.x, rayColor);
+        return hit;
     }
 
     private void Jump()
@@ -53,13 +53,6 @@ public class Movement : MonoBehaviour
             Vector2 jump = new Vector2(0, jumpForce);
             playerRigid.AddForce(jump, ForceMode2D.Impulse);
         }
-        
-        /*if (Input.GetKeyDown(jumpKey) && _canJump)
-        {
-            Vector2 jump = new Vector2(0, jumpForce);
-            playerRigid.AddForce(jump, ForceMode2D.Impulse);
-            _canJump = false;
-        }*/
     }
     
 
@@ -68,13 +61,4 @@ public class Movement : MonoBehaviour
         Move();
         Jump();
     }
-
-    /*private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Player"))
-        {
-            _canJump = true;
-        }
-    }
-    */
 }
